e0f0f0d5791bcbfd99cc2264b2c9fff956e7f092
Sean Lange <seanlange74@gmail.com>
2026-01-22 23:02:23 -0500

    Initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 
 import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	// Enable React to support React JSX components.
 	integrations: [react()],
 });
